31c6a8074ba56d6ae8efe0f976d37e0b4315d2e8
scratchminer <scratchminer@gmail.com>
2024-01-23 11:56:54 -0500

    small fix for when cycles is exactly 2

--- src/pwrnoise.c
+++ src/pwrnoise.c
@@ -70,7 +70,7 @@ void pwrnoise_noise_step(noise_channel_t *chan, uint16_t cycles) {
 	}
 	
 	chan->octave_counter += cycles;
-	if (cycles > 2 || !(((chan->octave_counter - 1) >> chan->octave) & 0x0001) && ((chan->octave_counter >> chan->octave) & 0x0001)) {
+	if (cycles >= 2 || !(((chan->octave_counter - 1) >> chan->octave) & 0x0001) && ((chan->octave_counter >> chan->octave) & 0x0001)) {
 		chan->period_counter += (cycles >> (chan->octave + 1));
 		if (cycles == 1) ++chan->period_counter;
 		
@@ -156,7 +156,7 @@ void pwrnoise_slope_step(slope_channel_t *chan, uint16_t cycles, bool force_zero
 	}
 	
 	chan->octave_counter += cycles;
-	if (cycles > 2 || !(((chan->octave_counter - 1) >> chan->octave) & 0x0001) && ((chan->octave_counter >> chan->octave) & 0x0001)) {
+	if (cycles >= 2 || !(((chan->octave_counter - 1) >> chan->octave) & 0x0001) && ((chan->octave_counter >> chan->octave) & 0x0001)) {
 		chan->period_counter += (cycles >> (chan->octave + 1));
 		if (cycles == 1) ++chan->period_counter;
 		
